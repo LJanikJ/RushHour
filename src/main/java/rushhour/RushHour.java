cc6b070e58b36a034a6763402e8ac47893b88fdd
Lukas Janik-Jones <ljanikjo@uoguelph.ca>
2021-01-06 00:52:06 -0500

    Began commenting

--- src/main/java/rushhour/RushHour.java
+++ src/main/java/rushhour/RushHour.java
@@ -19,41 +19,58 @@ public class RushHour {
     private ArrayList<GameState> stateQueue;
     private ArrayList<GameState> optimalPath;
 
+    /**
+    Default constructor.
+    */
     public RushHour() {
+        //Declare all arraylists
         blocks = new ArrayList<Block>();
         allStates = new HashMap<Long, GameState>();
         stateQueue = new ArrayList<GameState>();
         optimalPath = new ArrayList<GameState>();
 
-        solutionDepth = 10000;
+        //Set solution depth
+        solutionDepth = 100;
 
+        //Set default exit location
         exitLocation = new Point(-1, -1);
     }
 
+    /**
+    Constructor with parser.
+    @param parser The RushHourParser object
+    */
     public RushHour(RushHourParser parser) {
+        //Declare all arraylists
         blocks = new ArrayList<Block>();
         allStates = new HashMap<Long, GameState>();
         stateQueue = new ArrayList<GameState>();
         optimalPath = new ArrayList<GameState>();
 
-        solutionDepth = 10000;
+        //Set important values
+        solutionDepth = 100;
         width = parser.getWidth();
         height = parser.getHeight();
 
+        //Iteratively add all blocks from parser
         Map currBlock = parser.nextBlock();
         while (currBlock != null) {
             addBlock(currBlock);
             currBlock = parser.nextBlock();
         }
 
+        //Set exit location
         exitLocation = findExitLocation();
 
+        //Begin creating the game tree
         makeGameTree();
     }
 
     private Point findExitLocation() {
+        //Search all blocks for the primary block
         for (Block block : blocks) {
             if (block.getPrimary()) {
+                //Calculate the exit location based on the block direction/length
                 if (block.getDirection().equals("horizontal")) {
                     int exitX = width - block.getLength();
                     int exitY = block.getXyLocation().y;
@@ -68,38 +85,60 @@ public class RushHour {
             }
         }
 
+        //This should never be reached, only reached with no primary
         System.out.println("Exit error");
-        return new Point(-1, -1);
+        System.exit(1);
     }
 
     private void addBlock(Map toAdd) {
+        //Allocate space by creating new block object
         Block block = new Block();
 
+        //Find the x and y position of the upper-left point of the block
         int x = Integer.parseInt(toAdd.get("x").toString());
         int y = Integer.parseInt(toAdd.get("y").toString());
 
+        //Set all variables for the block
         block.setXyLocation(new Point (x, y));
         block.setId(toAdd.get("id").toString());
         block.setLength(Integer.parseInt(toAdd.get("length").toString()));
         block.setDirection(toAdd.get("direction").toString());
         block.setPrimary(toAdd.get("primary").toString());
 
+        //Use location, direction, and length to create an array of points
+        //Forming the blocks area
         block.createArea();
 
+        //Add block to arraylist
         blocks.add(block);
     }
 
+    /**
+    Generates all gameStates and searches through them until it finds a solution.
+    */
     public void makeGameTree() {
+        //Record the initial state as the root of the game tree
         startingState = new GameState(this, blocks, 0);
 
+        //Iterate through the gameState queue, in order of depth
         while (stateQueue.size() > 0) {
+            //Add state to hashmap
             allStates.put(stateQueue.get(0).getHashKey(), stateQueue.get(0));
+            //Check if the state is solved, if not then add moves to queue
             stateQueue.get(0).checkSolved();
+            //If solved, exit the while loop
             if (stateQueue.get(0).isSolved()) {
                 break;
             }
 
+            //Remove the state from front of queue
             stateQueue.remove(0);
+        }
+
+        //If the queue exhasted all possible moves, then the puzzle cannot be solved
+        if (stateQueue.size() == 0) {
+            System.out.println("Puzzle has no solution");
+            System.exit(1);
         }
     }
 
